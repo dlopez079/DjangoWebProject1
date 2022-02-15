abf8adf8bfc84770ac36221f918c5338695b62f3
David Lopez <50810168+dlopez079@users.noreply.github.com>
2022-02-15 08:55:00 -0500

    Debug Modification Debug is great for development.   For a production environment, though, this is a really bad idea, because it leaks lots of information about your project: excerpts of your source code, local variables, settings, libraries used, etc.  I turned it off.

--- DjangoWebProject1/settings.py
+++ DjangoWebProject1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fbf8bb71-3800-4df2-8524-872d1fbadd8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mm-djangowebproject1.azurewebsites.net']
 
